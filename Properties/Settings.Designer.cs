b2fed952c4a2c512ed13d3c37c8d56f7c9e59d17
DaltonChris <daltonchris2@gmail.com>
2024-05-14 00:22:27 +0930

    vs cooking out but is working

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPF_Desktop_Fren.Properties {
+namespace Desktop_Frens.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
